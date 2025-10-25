1a5ee901de901b14747fc3486de89001ad23553f
youyacoder <hero_cloudy@126.com>
2025-10-25 14:49:56 +0800

    feat: [portal]预加载子应用

--- voc-portal/app/plugins/micro-plugin.client.ts
+++ voc-portal/app/plugins/micro-plugin.client.ts
@@ -1,6 +1,10 @@
 import WujieVue from 'wujie-vue3'
 
+const { setupApp, preloadApp } = WujieVue
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(WujieVue)
+  setupApp({ name: 'voc-editor', url: 'http://localhost:8081', exec: true, sync: true })
+  preloadApp({ name: 'voc-editor' })
   console.log('micro plugin loaded...')
 })
